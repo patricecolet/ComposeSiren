2c40fa76d1d1cbf8bf373de8ea4f3c3cc5556c50
Joseph Larralde <joseph.larralde@gmail.com>
2026-03-24 10:56:57 +0100

    GUI + Listener for reset and select resources btns

--- Source/ComposeSirenCore/Components/MainButtonsComponent.h
+++ Source/ComposeSirenCore/Components/MainButtonsComponent.h
@@ -7,37 +7,136 @@
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
-class ResetComponent : public juce::Component
+class MainButtonsComponent : public juce::Component,
+                             public juce::TextButton::Listener
 {
-    juce::TextButton resetButton;
-    juce::TextButton selectResourcesButton;
-
 public:
-    ResetComponent()
+    class Listener {
+    public:
+        virtual ~Listener() = default;
+        virtual void resetSiren() = 0;
+        virtual void selectedNewResourcesPath(const std::string&) = 0;
+        virtual std::string getResourcesPath() = 0;
+    };
+
+    MainButtonsComponent(Listener& l) : listener(l)
     {
-        resetButton.setColour(juce::TextButton::buttonColourId, juce::Colours::red);
-        // resetButton.setColour(juce::TextButton::buttonOnColourId , juce::Colours::grey);
-        resetButton.setColour(juce::TextButton::textColourOffId , juce::Colours::black);
+        auto darkTransBg = juce::Colour{0xff283541}.withAlpha(0.95f);
+
+        resetButton.setColour(juce::TextButton::buttonColourId, juce::Colours::darkred);
+        resetButton.setColour(juce::TextButton::textColourOffId , juce::Colours::whitesmoke);
         resetButton.setButtonText ("Reset");
+        resetButton.addListener(this);
         addAndMakeVisible(resetButton);
 
-        selectResourcesButton.setColour(juce::TextButton::buttonColourId, juce::Colours::whitesmoke);
-        selectResourcesButton.setColour(juce::TextButton::textColourOffId , juce::Colours::black);
+        selectResourcesButton.setColour(juce::TextButton::buttonColourId, darkTransBg);
+        selectResourcesButton.setColour(juce::TextButton::textColourOffId , juce::Colours::whitesmoke);
         selectResourcesButton.setButtonText("Set resources directory");
+        selectResourcesButton.addListener(this);
+#ifdef COMPOSESIREN_DEV_BUILD
         addAndMakeVisible(selectResourcesButton);
+#endif
     }
 
-    ~ResetComponent() = default;
+    ~MainButtonsComponent() override = default;
 
     void paint(juce::Graphics& g) override
     {
-
+        g.setColour(juce::Colour{0xff314159});
+        g.fillRect(getLocalBounds().toFloat());
     }
 
     void resized() override
     {
+        const int margin = 5;
 
+        auto bounds = getLocalBounds();
+        bounds.removeFromTop(margin);
+        bounds.removeFromRight(margin);
+        bounds.removeFromLeft(margin);
+
+        juce::FlexBox fb;
+
+        fb.flexDirection = juce::FlexBox::Direction::row;
+        fb.flexWrap = juce::FlexBox::Wrap::noWrap;
+        // fb.alignItems = juce::FlexBox::AlignItems::center;
+        fb.alignItems = juce::FlexBox::AlignItems::flexEnd;
+        fb.alignContent = juce::FlexBox::AlignContent::spaceBetween;
+
+        const int menuHeight = bounds.getHeight();
+        juce::FlexItem item;
+
+        // Left button /////////////////////////////////////////////////////////
+        // fb.alignContent = juce::FlexBox::AlignContent::flexStart;
+        // fb.justifyContent = juce::FlexBox::JustifyContent::flexStart;
+        //
+        // item = juce::FlexItem(resetButton).withMinWidth(75)
+        //                                   .withMinHeight(menuHeight)
+        //                                   .withFlex(0,0);
+        // fb.items.add(item);
+        // fb.performLayout(bounds);
+        // fb.items.clear();
+
+        // Right button ////////////////////////////////////////////////////////
+        // fb.alignContent = juce::FlexBox::AlignContent::flexEnd;
+        // fb.justifyContent = juce::FlexBox::JustifyContent::flexEnd;
+        //
+        // item = juce::FlexItem(selectResourcesButton).withMinWidth(230)
+        //                                             .withMinHeight(menuHeight)
+        //                                             .withFlex(0,1);
+        // fb.items.add(item);
+        // fb.performLayout(bounds);
+
+        // All buttons right ///////////////////////////////////////////////////
+        fb.alignContent = juce::FlexBox::AlignContent::flexEnd;
+        fb.justifyContent = juce::FlexBox::JustifyContent::flexEnd;
+
+        item = juce::FlexItem(selectResourcesButton).withMinWidth(230)
+                                                    .withMinHeight(menuHeight)
+                                                    .withFlex(0,1);
+        fb.items.add(item);
+        item = juce::FlexItem(resetButton).withMinWidth(75)
+                                          .withMinHeight(menuHeight)
+                                          .withFlex(0,0);
+        item.margin = juce::FlexItem::Margin(0.f, 0.f, 0.f, (float) margin);
+        fb.items.add(item);
+        fb.performLayout(bounds);
     }
+
+    void buttonClicked(juce::Button* btn) override
+    {
+        if (btn == &resetButton) {
+            listener.resetSiren();
+            return;
+        }
+
+        if (btn == &selectResourcesButton) {
+            const std::string resourcesPath = listener.getResourcesPath();
+            fileChooser = std::make_unique<juce::FileChooser>(
+                "Select a file", juce::File(resourcesPath), ""
+            );
+
+            auto flags =
+                juce::FileBrowserComponent::openMode
+                | juce::FileBrowserComponent::canSelectDirectories;
+
+            fileChooser->launchAsync(flags, [this](const juce::FileChooser& chooser) {
+                // get the result to update resourcesPath
+                juce::File newResourcesPath = chooser.getResult();
+                listener.selectedNewResourcesPath(
+                    newResourcesPath.getFullPathName().toStdString()
+                );
+            });
+        }
+    }
+
+private:
+    Listener& listener;
+
+    juce::TextButton resetButton;
+    juce::TextButton selectResourcesButton;
+
+    std::unique_ptr<juce::FileChooser> fileChooser;
 };
 
 
