bd97a39e6b5e40ad8bf3732368dbbf22efe9dbaf
Joseph Larralde <joseph.larralde@gmail.com>
2026-05-06 12:22:07 +0200

    fixed too small buffer size (use a queue instead)

--- Source/ComposeSirenCore/MidiScheduler.h
+++ Source/ComposeSirenCore/MidiScheduler.h
@@ -8,37 +8,59 @@
 #include <juce_audio_basics/juce_audio_basics.h>
 
 class MidiScheduler {
-public:
-    void schedule(const juce::MidiMessage& msg, int offset) {
-        if (count >= maxEvents) { return; }
-        // x++ : eval, then increment => ok
-        events[count++] = { msg, offset };
-    }
-
-    void flush(juce::MidiBuffer& buffer,
-               AnyOrOneBasedMidiChannel outch = AnyOrOneBasedMidiChannel::any()) {
-        for (int i = 0; i < count; ++i) {
-            if (!outch.isAny) {
-                events[i].msg.setChannel(outch.channel.oneBased);
-            }
-            buffer.addEvent(events[i].msg, events[i].offset);
-        }
-        count = 0;
-    }
-
-    void reset() {
-        count = 0;
-    }
-
-private:
     struct Event {
         juce::MidiMessage msg;
         int offset;
     };
 
-    static constexpr int maxEvents = 64;
-    Event events[maxEvents] = {};
-    int count = 0;
+    // maxEvents == 64 is not enough for SirenOrchestra, but 128
+    // seems fine (all parameters are recalled on startup).
+    // We could just keep the array solution with the increased
+    // maxEvents value, but this feels wrong, so here is the
+    // std::queue version (everything happens in the audio thread)
+    // todo : estimate the overhead of using queue vs array
+
+    // static constexpr int maxEvents = 128;
+    // Event events[maxEvents] = {};
+    // int count = 0;
+
+    std::queue<Event> events;
+
+public:
+    void schedule(const juce::MidiMessage& msg, int offset) {
+        // if (count >= maxEvents) { return; }
+        // // x++ : eval, then increment => ok
+        // events[count++] = { msg, offset };
+
+        events.emplace(Event{msg, offset});
+    }
+
+    void flush(juce::MidiBuffer& buffer,
+               AnyOrOneBasedMidiChannel outch = AnyOrOneBasedMidiChannel::any()) {
+        // for (int i = 0; i < count; ++i) {
+        //     if (!outch.isAny) {
+        //         events[i].msg.setChannel(outch.channel.oneBased);
+        //     }
+        //     buffer.addEvent(events[i].msg, events[i].offset);
+        // }
+        // count = 0;
+
+        while (!events.empty()) {
+            auto e = events.front();
+            if (!outch.isAny) {
+                e.msg.setChannel(outch.channel.oneBased);
+            }
+            buffer.addEvent(e.msg, e.offset);
+            events.pop();
+        }
+    }
+
+    void reset() {
+        // count = 0;
+
+        std::queue<Event> empty;
+        std::swap(events, empty);
+    }
 };
 
 #endif //COMPOSESIREN_MIDISCHEDULER_H
