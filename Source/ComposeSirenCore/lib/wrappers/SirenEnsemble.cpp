eb580e8f01e580378ff691360271a30bd726b5d2
Joseph Larralde <joseph.larralde@gmail.com>
2026-05-08 15:59:57 +0200

    implement SirenTrack and SirenEnsemble classes

--- Source/ComposeSirenCore/lib/wrappers/SirenEnsemble.cpp
+++ Source/ComposeSirenCore/lib/wrappers/SirenEnsemble.cpp
@@ -4,7 +4,163 @@
 
 #include "SirenEnsemble.h"
 
-SirenEnsemble::SirenEnsemble()
-{
+#include <ranges>
 
+//------------------------------------------------------------------------------
+// SIREN TRACK /////////////////////////////////////////////////////////////////
+//------------------------------------------------------------------------------
+
+SirenTrack::SirenTrack(sirenId id, const std::string& resourcesPath)
+{
+    SirenVoice::setSirenId(id, resourcesPath);
+    computePanningGains();
+}
+
+// SirenTrack::~SirenTrack() {}
+
+void SirenTrack::setSampleRate(double sr) {
+    SirenVoice::setSampleRate(sr);
+    gr.setSampleRate(sr);
+    pr.setSampleRate(sr);
+}
+void SirenTrack::setPanning(float p) { panning.store(p,std::memory_order_relaxed); }
+void SirenTrack::setOutputGain(float g) { gain.store(g, std::memory_order_relaxed); }
+
+// call this at the beginning of the audio callback
+void SirenTrack::beginProcessBlock() {
+    SirenVoice::beginProcessBlock();
+    gr.setTarget(gain.load(std::memory_order_relaxed));
+    pr.setTarget(panning.load(std::memory_order_relaxed));
+}
+
+// call this for each sample during the audio callback
+void SirenTrack::process(float *l, float* r) {
+    gr.process(g);
+    // only recompute panning gains if needed (i.e if we are ramping) :
+    if (pr.process(p)) { computePanningGains(); }
+    float s = SirenVoice::process() * g;
+    *l = s * lp;
+    *r = s * rp;
+}
+
+// we can add sqrt panning law and others later if we like
+void SirenTrack::computePanningGains() {
+    p = (p + 1.0f) * 0.5f; // [-1;1] to [0;1]
+    lp = std::cos(p * pi_2);
+    rp = std::cos((1.0f - p) * pi_2);
+}
+
+//------------------------------------------------------------------------------
+// SIREN ENSEMBLE //////////////////////////////////////////////////////////////
+//------------------------------------------------------------------------------
+
+SirenEnsemble::SirenEnsemble(const std::vector<sirenId>& ids,
+                             const std::string& resourcesPath) {
+    for (auto id : ids) {
+        tracks[id] = std::make_unique<SirenTrack>(id, resourcesPath);
+    }
+}
+
+bool SirenEnsemble::areSirensLoading() const {
+    for (auto& t : tracks | std::views::values) {
+        if (t->isSirenLoading()) { return true; }
+    }
+    return false;
+}
+
+bool SirenEnsemble::getRawSirenHandles() const {
+    for (auto& t : tracks | std::views::values) {
+        if (!t->getRawSirenHandle()) { return false; }
+    }
+    return true;
+}
+
+std::vector<sirenId> SirenEnsemble::getSirenIds() const {
+    auto keys = std::views::keys(tracks);
+    std::vector<sirenId> ids{keys.begin(), keys.end()};
+    return ids;
+}
+
+void SirenEnsemble::addListener(SirenVoice::Listener* listener) {
+    for (auto& t : tracks | std::views::values) { t->addListener(listener); }
+}
+
+void SirenEnsemble::removeListener(SirenVoice::Listener* listener) {
+    for (auto& t : tracks | std::views::values) { t->removeListener(listener); }
+}
+
+void SirenEnsemble::removeAllListeners() {
+    for (auto& t : tracks | std::views::values) { t->removeAllListeners(); }
+}
+
+void SirenEnsemble::notifyListeners() {
+    for (auto& t : tracks | std::views::values) { t->notifyListeners(); }
+}
+
+void SirenEnsemble::setSampleRate(double sr) {
+    for (auto& t : tracks | std::views::values) { t->setSampleRate(sr); }
+    vr.setSampleRate(sr);
+}
+
+void SirenEnsemble::update() {
+    for (auto& t : tracks | std::views::values) { t->update(); }
+}
+
+void SirenEnsemble::stop(std::optional<sirenId> id) {
+    if (id.has_value()) {
+        tracks.at(id.value())->stop();
+    } else {
+        for (auto& t : tracks | std::views::values) { t->stop(); }
+    }
+}
+
+void SirenEnsemble::setPanning(sirenId id, float p) {
+    tracks.at(id)->setPanning(p);
+}
+
+void SirenEnsemble::setOutputGain(sirenId id, float g) {
+    tracks.at(id)->setOutputGain(g);
+}
+
+void SirenEnsemble::setMasterVolume(float v) {
+    masterVolume.store(v, std::memory_order_relaxed);
+}
+
+void SirenEnsemble::handleMidi(int status, int value1, int value2) {
+    // high nibble goes from 8 to F (ms bit is always on)
+    // low nibble is the channel so we just check it and forward
+    // the message to the appropriate siren
+    if (status > 0xFF) { return; } // sanitize
+    OneBasedMidiChannel channel = {.oneBased = (status & 0x0F) + 1};
+
+    auto it = sirenPropertiesByChannel.find(channel);
+    if (it != sirenPropertiesByChannel.end()) {
+        auto id = it->second->id;
+        tracks.at(id)->handleMidi(status, value1, value2);
+    }
+}
+
+void SirenEnsemble::beginProcessBlock() {
+    for (auto& t : tracks | std::views::values) { t->beginProcessBlock(); }
+    vr.setTarget(masterVolume.load(std::memory_order_relaxed));
+}
+
+void SirenEnsemble::process(float* l, float* r) {
+    *l = *r = 0;
+    float tmpl = 0;
+    float tmpr = 0;
+    for (auto& t : tracks) {
+        t.second->process(&tmpl, &tmpr);
+        *l += tmpl;
+        *r += tmpr;
+    }
+    vr.process(v);
+    *l *= v;
+    *r *= v;
+    // *l /= tracks.size();
+    // *r /= tracks.size();
+}
+
+void SirenEnsemble::deleteDiscarded() const {
+    for (auto& t : tracks | std::views::values) { t->deleteDiscarded(); }
 }
--- Source/ComposeSirenCore/lib/wrappers/SirenEnsemble.h
+++ Source/ComposeSirenCore/lib/wrappers/SirenEnsemble.h
@@ -5,10 +5,90 @@
 #ifndef COMPOSESIREN_SIREN_ENSEMBLE_H
 #define COMPOSESIREN_SIREN_ENSEMBLE_H
 
+#include "SirenVoice.h"
+#include "../dsp/rampe.h"
+#include "../definitions/parameterDefinitions.h"
+
+const float pi_2 = 1.57079632679489661923f;
+
+//------------------------------------------------------------------------------
+// SIREN TRACK /////////////////////////////////////////////////////////////////
+
+// shall we add integrated smooth filters for pan/gain controls ?
+class SirenTrack : public SirenVoice
+{
+    std::atomic<float> gain{1.0f};    // identity
+    float g{0.0f};
+    rampe<float> gr{0, 10};
+
+    std::atomic<float> panning{0.0f}; // centred
+    float p{0.0f};
+    rampe<float> pr{1, 10};
+
+    float lp{0.0f};
+    float rp{0.0f};
+
+public:
+    SirenTrack(sirenId id, const std::string& resourcesPath);
+    ~SirenTrack() override = default;
+
+    void setSampleRate(double sr) override;
+    void setPanning(float p);
+    void setOutputGain(float g);
+
+    // call this at the beginning of the audio callback
+    void beginProcessBlock() override;
+
+    // call this for each sample during the audio callback
+    void process(float *l, float* r);
+
+private:
+    // we can add sqrt panning law and others later if we like
+    void computePanningGains();
+};
+
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// SIREN ENSEMBLE //////////////////////////////////////////////////////////////
+
 class SirenEnsemble {
 public:
-    SirenEnsemble();
+    SirenEnsemble(const std::vector<sirenId>& ids,
+                  const std::string& resourcesPath);
     ~SirenEnsemble() = default;
+
+    bool areSirensLoading() const;
+    bool getRawSirenHandles() const;
+    std::vector<sirenId> getSirenIds() const;
+
+    void addListener(SirenVoice::Listener* listener);
+    void removeListener(SirenVoice::Listener* listener);
+    void removeAllListeners();
+    void notifyListeners();
+
+    void update();
+    void setSampleRate(double sr);
+    void stop(std::optional<sirenId> id = std::nullopt);
+
+    void setPanning(sirenId id, float p);
+    void setOutputGain(sirenId id, float g);
+    // bool getIsNoteOn(sirenId id);
+    // float getCurrentPitch(sirenId);
+    void setMasterVolume(float v);
+    void handleMidi(int status, int value1, int value2);
+    void beginProcessBlock();
+    void process(float* l, float* r);
+    void deleteDiscarded() const;
+
+private:
+    std::atomic<float> masterVolume = 1.0f;
+    rampe<float> vr{1, 10};
+    float v{0};
+
+    std::map<sirenId, std::unique_ptr<SirenTrack>> tracks;
 };
+
+//------------------------------------------------------------------------------
 
 #endif //COMPOSESIREN_SIREN_ENSEMBLE_H
