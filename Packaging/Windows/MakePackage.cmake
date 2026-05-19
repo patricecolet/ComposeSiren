fa41fa71e3ebf82cb6d78be20f2a4a417d3aabf4
Joseph Larralde <joseph.larralde@gmail.com>
2026-05-19 14:32:57 +0200

    NSIS working but not registering standalones

--- Packaging/Windows/MakePackage.cmake
+++ Packaging/Windows/MakePackage.cmake
@@ -191,3 +191,11 @@ endif()
 if(NOT "${PLUGIN_RESOURCES_DIR}" STREQUAL "")
   cpack_add_component("Resources" DISPLAY_NAME "Shared plugin data" REQUIRED)
 endif()
+
+message("${COMPONENTS_LIST}")
+add_custom_target(dist DEPENDS ${COMPONENTS_LIST})
+add_custom_command(TARGET dist POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E echo "== Running CPack (NSIS) =="
+        COMMAND "${CMAKE_CPACK_COMMAND}" -C "$<IF:$<CONFIG:>,${CMAKE_BUILD_TYPE},$<CONFIG>>"
+        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+)
